a19d7ed031919d0fbabc87b8fe18f81bb4c0e3ff
viveklakshminarayanan-svg <vivek.lakshminarayanan@gmail.com>
2026-05-16 20:53:38 +0200

    Update navigation links and logo structure

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,26 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './Nav.module.css'
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
-      <Link href="/" className={styles.logo}>VL</Link>
+      <Link href="/" className={styles.logoWrap}>
+        <Image
+          src="/logo.png"
+          alt="Vivek Lakshminarayanan"
+          width={160}
+          height={60}
+          style={{ objectFit: 'contain' }}
+          priority
+        />
+      </Link>
       <div className={styles.links}>
         <Link href="/about">About</Link>
-        <Link href="/experience">Experience</Link>
-        <Link href="/cv">CV</Link>
-        <Link href="/blog">Blog</Link>
+        <Link href="/experience">Work</Link>
+        <Link href="/blog">Writing</Link>
         <Link href="/contact">Contact</Link>
+        <Link href="/contact" className={styles.cta}>Let's talk</Link>
       </div>
     </nav>
   )
